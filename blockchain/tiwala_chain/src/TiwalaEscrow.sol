edaf1df1ee5ac1b4eae9c5bef538d030bc52a442
rustumsk <rustumarellano@gmail.com>
2026-02-25 21:48:30 +0800

    add/ enums,mapping and constructor for the escrow wallet

--- blockchain/tiwala_chain/src/TiwalaEscrow.sol
+++ blockchain/tiwala_chain/src/TiwalaEscrow.sol
@@ -1,6 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-pragma 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract TiwalaEscroe{
+contract TiwalaEscrow {
     
+    address public moderator;
+    address public usdtToken;
+    uint256 public jobCounter;
+
+    enum JobStatus {
+        Created,
+        Funded,
+        InProgress,
+        Submitted,
+        Disputed,
+        Completed,
+        Refunded
+    }
+
+    struct EscrowJob {
+        address employer;
+        address freelancer;
+        uint256 amount;
+        JobStatus status;
+        bytes32 contractHash;
+    }
+
+    mapping(uint256 => EscrowJob) public jobs;
+    mapping(address => uint256[]) public employerJobs;
+    mapping(address => uint256[]) public freelancerJobs;
+
+    constructor(address _usdtToken) {
+        moderator = msg.sender;
+        usdtToken = _usdtToken;
+    }
 }
