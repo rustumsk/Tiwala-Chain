8085150caa8808b084e435cf3a134b86a37ad513
rustumsk <rustumarellano@gmail.com>
2026-04-20 22:11:50 +0800

    refactor: add posting mapper policy and validation

---
+++ backend/TiwalaChain.API/common/PostingConstants.cs
@@ -0,0 +1,39 @@
+public static class PostingConstants
+{
+    public static readonly HashSet<string> ValidCategories =
+    [
+        "development",
+        "design",
+        "marketing",
+        "writing",
+        "admin_support",
+        "customer_support",
+        "video_media",
+        "blockchain",
+        "ai_data",
+        "product_strategy",
+    ];
+
+    public static readonly HashSet<string> ValidExperienceLevels =
+    [
+        "entry",
+        "intermediate",
+        "expert",
+    ];
+
+    public static readonly HashSet<string> ValidJobTypes =
+    [
+        "fixed_price",
+    ];
+
+    public static readonly HashSet<string> ValidBudgetTypes =
+    [
+        "fixed",
+        "range",
+    ];
+
+    public static readonly HashSet<string> ValidVisibility =
+    [
+        "public",
+    ];
+}
